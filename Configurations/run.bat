12846cb4fc94056b40d8638911ed2db7f5050960
twinkalukey135-ship-it <twinkalukey135@gmail.com>
2026-02-01 20:11:10 +0530

    Update run.bat
    updating run.bat file at global

--- Configurations/run.bat
+++ Configurations/run.bat
@@ -1,2 +1,3 @@
 pytest -n=auto --html-Html_Reports\mychromereport_1st_feb_26.html --browser chrome
 pytest -n=auto --html-Html_Reports\mychromereport_1st_feb_26.html --browser firefox
+pytest -n=auto --html-Html_Reports\myedgereport_1st_feb_26.html --browser edge
